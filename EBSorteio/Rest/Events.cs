f061a56fe3ed1d38098ecd2a37e219d1aabc6014
Igor Koubetch <i.koubetch@gmail.com>
2015-12-19 13:08:10 -0200

    Json Events

---
+++ EBSorteio/Rest/Events.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace EBSorteio.Rest
+{
+	public class Events
+	{
+		[JsonProperty("text")]
+		public string Name { get; set; }
+	}
+}
+
---
+++ EBSorteio/Rest/UserEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace EBSorteio.Rest
+{
+	public class UserEvent
+	{
+		[JsonProperty("name")]
+		public Events Events { get; set; }
+
+	}
+}
+
