c307906f5154310e5618520a629a23d6864de7d9
Ione Souza Junior <junior@ionixjunior.com.br>
2015-12-19 12:51:56 -0200

    Adicionado carregamento de participantes

--- EBSorteio/View/AttendeesView.xaml.cs
+++ EBSorteio/View/AttendeesView.xaml.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using EBSorteio.ViewModel;
 
 namespace EBSorteio.View
 {
 	public partial class AttendeesView : ContentPage
 	{
+		private AttendeesViewModel ViewModel { get; set; }
+
 		public AttendeesView ()
 		{
 			InitializeComponent ();
+		}
+
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+
+			if (ViewModel == null) 
+			{
+				ViewModel = new AttendeesViewModel ();
+				BindingContext = ViewModel;
+			}
+
+			if (ViewModel.Attendees == null) 
+			{
+				ViewModel.Load ();
+			}
 		}
 	}
 }
