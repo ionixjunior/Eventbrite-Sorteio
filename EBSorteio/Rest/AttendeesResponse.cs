5ed2445f06b74eafbde98f703c7450a7449a945c
Ione Souza Junior <junior@ionixjunior.com.br>
2015-12-19 12:21:26 -0200

    Adicionado classes para mapeamento da rest

--- EBSorteio/Rest/AttendeesResponse.cs
+++ EBSorteio/Rest/AttendeesResponse.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EBSorteio.Rest
 {
 	public class AttendeesResponse
 	{
-		
+		public List<Attendee> Attendees { get; set; }
 	}
 }
 
---
+++ EBSorteio/Rest/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EBSorteio.Rest
+{
+	public class Profile
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string Email { get; set; }
+	}
+}
+
