809dfa57d294435fed1896838d6e87d827312b71
Ione Souza Junior <junior@ionixjunior.com.br>
2015-12-31 01:53:02 -0200

    Adicionado botão oauth na página home; Direcionado aplicativo para abrir na página home e controlar se existe variável de sessão

--- EBSorteio/View/HomeView.xaml.cs
+++ EBSorteio/View/HomeView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using EBSorteio.Common;
 
 namespace EBSorteio.View
 {
@@ -10,6 +11,27 @@ namespace EBSorteio.View
 		public HomeView ()
 		{
 			InitializeComponent ();
+		}
+
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+
+			if (SessionManager.Get (SessionName.OAuthToken) != null) 
+			{
+				Xamarin.Forms.Application.Current.MainPage = new NavigationPage (
+					new EventsView()
+				);
+			}
+		}
+
+		public async void LoginEventbrite(object sender, EventArgs args)
+		{
+			await Navigation.PushModalAsync (
+				new NavigationPage(
+					new OAuthView()
+				)
+			);
 		}
 	}
 }
