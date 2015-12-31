2bbccd04604da97b4f0e5f24e065176babc17536
Ione Souza Junior <junior@ionixjunior.com.br>
2015-12-30 22:15:52 -0200

    Atualizado para Xamarin Forms 2.0 e adicionado SessionManager

---
+++ EBSorteio/Common/SessionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace EBSorteio.Common
+{
+	public class SessionManager
+	{
+		public static object Get(string key)
+		{
+			if (Contains (key).Equals (false)) 
+			{
+				return null;
+			}
+
+			return Application.Current.Properties [key];
+		}
+
+		public static async Task SetAsync(string key, object value)
+		{
+			Application.Current.Properties [key] = value;
+			await Application.Current.SavePropertiesAsync ();
+		}
+
+		public static async Task Clean()
+		{
+			Application.Current.Properties.Clear ();
+			await Application.Current.SavePropertiesAsync ();
+		}
+
+		private static bool Contains(string key)
+		{
+			if (Application.Current.Properties.ContainsKey (key)) 
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
+
